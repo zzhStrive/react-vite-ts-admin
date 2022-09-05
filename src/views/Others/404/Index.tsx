d1cba77c9e36a1c203f8602dec84077b886b770b
zzh <1095959430@qq.com>
2022-09-05 16:48:26 +0800

    暂停开发

--- src/views/Others/404/Index.tsx
+++ src/views/Others/404/Index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import img404 from '@/assets/images/404.jpg'
+import img404 from '@/assets/404.jpg'
 
-const View404 = () => (
-    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
-        <img src={img404} alt='' />
-    </div>
+const View404: React.FC<{}> = () => (
+
+  <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+    <img src={img404} alt='' />
+  </div>
 )
 
 export default View404
--- src/views/Others/500/Index.tsx
+++ src/views/Others/500/Index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import img500 from '@/assets/images/500.png'
+import img500 from '@/assets/500.png'
 
 const View500 = () => (
-    <div style={{ textAlign: 'center' }}>
-        <img src={img500} alt='' />
-    </div>
+  <div style={{ textAlign: 'center' }}>
+    <img src={img500} alt='' />
+  </div>
 )
 
 export default View500
